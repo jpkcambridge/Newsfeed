53f3ba21ed3aceabdbed7737821921e2db23f354
John Kelleher <johnpkelleher@gmail.com>
2012-09-02 12:44:30 -0400

    initial commit

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,20 @@
+class StaticController < ApplicationController
+  def home
+    require 'rss'
+    require 'open-uri'
+#    @rss = RSS::Parser.parse open('http://slashdot.org/index.rdf'),false
+    wp_url = 'http://feeds.washingtonpost.com/rss/politics'
+    nyt_url = 'http://www.nytimes.com/services/xml/rss/nyt/World.xml'
+    url = 'http://www.ruby-lang.org/en/feeds/news.rss'
+    open(url) do |rss|
+      @feed = RSS::Parser.parse(rss)
+    end
+    open(nyt_url) do |rss|
+      @nyt_feed = RSS::Parser.parse(rss)
+    end
+    open(wp_url) do |rss|
+      @wp_feed = RSS::Parser.parse(rss, false)
+    end
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsfeed::Application.config.secret_token = '8bcdf5061aba701fe25a40baaffbf8e0f3d58bc6f7626642de338f4424a175841a51c772e555b897e82022a525c9ba751d053162737a24311b48ecb7d9c683b0'
