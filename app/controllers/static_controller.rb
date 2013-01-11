03a2ae8a08f028b8f93e094a7ef459352e3d2d75
John Kelleher <johnpkelleher@gmail.com>
2013-01-10 21:00:47 -0500

    upgraded rails

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,8 +3,8 @@ class StaticController < ApplicationController
     require 'rss'
     require 'open-uri'
 #    @rss = RSS::Parser.parse open('http://slashdot.org/index.rdf'),false
-    wp_url = 'http://feeds.washingtonpost.com/rss/politics'
-    nyt_url = 'http://www.nytimes.com/services/xml/rss/nyt/World.xml'
+    wp_url = 'http://feeds.washingtonpost.com/rss/national/energy-environment'
+    nyt_url = 'http://www.nytimes.com/services/xml/rss/nyt/EnergyEnvironment.xml'
     url = 'http://www.ruby-lang.org/en/feeds/news.rss'
     open(url) do |rss|
       @feed = RSS::Parser.parse(rss)
